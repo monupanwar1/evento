f9550fd037425016f7275b6a4b336e7a9b208597
kunal <monupanwar5655@gmail.com>
2025-03-22 10:09:56 +0530

    fetching data from prisma

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,7 @@
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 import { EventoEvent, PrismaClient } from "@prisma/client";
+import prisma from "./db";
 
 
 const primsa = new PrismaClient();
@@ -21,16 +22,22 @@ export function sleep(ms:number){
 
 
 export async function getEvents(city:string){
-  const response =await fetch(`https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`)
+  // const response =await fetch(`https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`)
 
-  const events:EventoEvent[]=await response.json();
+  // const events:EventoEvent[]=await response.json();
+
+  const events =await prisma.eventoEvent.findMany({
+    where:{
+      city:capitalize(city)
+    }
+  })
   return events;
 
 }
 export async function getEvent(slug:string){
   // const response =await fetch(`https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`)
 
-  const event =await primsa.EventoEvent.findUnique({
+  const event =await primsa.eventoEvent.findUnique({
     where:{
       slug:slug,
     },
